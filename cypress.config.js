b9091713a1bc03c14a023f071a2916bc70e543d8
Paul Gilzow <paul.gilzow@platform.sh>
2024-06-14 14:37:50 -0500

    debug env var issue with Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:1313/',
+    env: {
+      barfoo: "from config",
+      env: "local",
+    },
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+
+          return null
+        },
+      })
+    },
   },
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,7 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.task("log","Env is " + Cypress.env('env'))
+    cy.task("log","Barfoo is " + Cypress.env('barfoo'))
   })
 })
